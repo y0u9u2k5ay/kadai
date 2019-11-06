0256253b76a3c990cc481fa494f697cde3dc7725
y0u9u2k5ay <hengdao.youhua.787@gmail.com>
2019-11-06 05:28:27 +0000

    課題3を追加

--- kadai-2/index.js
+++ kadai-2/index.js
@@ -1,6 +1,6 @@
 const firstName = 'Taro'
 const secondMame = 'Yamada'
-console/log(firstName + secondName)
+console.log(firstName + secondName)
 
 const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
 console.log(sum)
@@ -12,3 +12,21 @@ console.log(hasDog || hasCat)
 console.log(hasDog && hasCat)
 //犬も猫も飼っていない
 console.log(!hasDog && !hasCat)
+
+const score = 70
+
+if(score >= 60) {
+    console.log('合格!')
+} else if(score >= 80) {
+    console.log('不合格!')
+｝
+
+const day = parseInt(ProcessingInstruction.argv[2])
+
+if (day % 3 === 0) {
+    console.log('あなたは慎重な性格です')
+}else if（day % 3 === 1)｛
+　　 console.log('あなたはまじめな性格です')
+}else if (day % 3 === 2) {
+    console.log('あなたは勇敢な性格です')
+｝
