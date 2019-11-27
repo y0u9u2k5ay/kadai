349650344689dd9d41baf646e5ff16900fc10826
y0u9u2k5ay <hengdao.youhua.787@gmail.com>
2019-11-27 05:13:43 +0000

    課題１を追加

---
+++ kadai-6/index-2.js
@@ -0,0 +1,26 @@
+const str1 = "abc"
+const str2 = "'abc'"
+const str3 = '"abc"'
+const str4 = '{"a": undefined}'
+const str5 = '{"a": 123}'
+const str6 = '{a: 123}'
+
+// str1　のパース結果
+try {
+    console.log(JSON.parse(str1))
+} catch (error) {
+    console.log(error)
+    // abc　という文字列は正しい JSON ではないから。
+    // 正しくは "abc"
+}
+
+// str2 のパース結果
+try {
+    console.log(JSON.parse(str2))
+} catch (error) {
+    console.log(error)
+    // 'abc' という文字列は正しい JSON ではないから。
+    // 正しくは "abc"
+}
+
+console.log(JSON.parse(str3))
